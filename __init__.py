587098d5a595d1462279f324cebdb054cfa87aac
vox <drewdebelo@gmail.com>
2024-07-22 13:10:05 +0100

    tryharder

--- __init__.py
+++ __init__.py
@@ -1,31 +1,3 @@
 from .node import *
-from .install import *
 
-NODE_CLASS_MAPPINGS = {
-    "CLAHEEnhancement": CLAHEEnhancement,
-    "HighPassFilter": HighPassFilter,
-    "EdgeDetection": EdgeDetection,
-    "CombineEnhancements": CombineEnhancements,
-    "AdaptiveThresholding": AdaptiveThresholding,
-    "MorphologicalOperations": MorphologicalOperations,
-    "GrayColorEnhancement": GrayColorEnhancement,
-    "ImprovedGrayColorEnhancement": ImprovedGrayColorEnhancement,
-    "TextureEnhancement": TextureEnhancement,
-    "DenoisingFilter": DenoisingFilter,
-    "FlexibleCombineEnhancements": FlexibleCombineEnhancements
-}
-
-NODE_DISPLAY_NAME_MAPPINGS = {
-    "CLAHEEnhancement": "CLAHE Enhancement",
-    "HighPassFilter": "High Pass Filter",
-    "EdgeDetection": "Edge Detection",
-    "CombineEnhancements": "Combine Enhancements",
-    "AdaptiveThresholding": "Adaptive Thresholding",
-    "MorphologicalOperations": "Morphological Operations",
-    "GrayColorEnhancement": "Gray Color Enhancement",
-    "ImprovedGrayColorEnhancement": "Improved Gray Color Enhancement",
-    "TextureEnhancement": "Texture Enhancement",
-    "DenoisingFilter": "Denoising Filter",
-    "FlexibleCombineEnhancements": "Flexible Combine Enhancements"
-}
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
